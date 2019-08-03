2c2a4e3bf61c0a014d5e6137f398b5305c5af102
lucasskoff <lucasskoff@gmail.com>
2019-08-03 11:24:05 -0500

    Changed gallery to photoset otherwise known as an album

--- Lucas-and-Liz/src/component/App.js
+++ Lucas-and-Liz/src/component/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const API_KEY = '&api_key=5f356f10c4288fa658f9be659201e7cd';
 const JSON = '&format=json&nojsoncallback=1';
-const GET_PHOTOS = 'https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos' + API_KEY + '&gallery_id=72157710018434537' + JSON;
+const GET_PHOTOS = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos' + API_KEY + '&photoset_id=72157710017493861' + JSON;
 const GET_PHOTO = 'https://api.flickr.com/services/rest/?method=flickr.photos.getInfo' + API_KEY;
 
 class App extends Component {
@@ -17,7 +17,7 @@ class App extends Component {
     componentDidMount() {
         fetch(GET_PHOTOS)
             .then(response => response.json())
-            .then(data => this.setState({ albums: data.photos.photo }));
+            .then(data => this.setState({ albums: data.photoset.photo }));
     }
 
     render() {
