2901de864ae1bb5220c8bcfdd711fb3b68e6e910
lucasskoff <lucasskoff@gmail.com>
2019-09-06 18:45:12 -0500

    Changed letter font sized for mobile support

--- Lucas-and-Liz/src/component/Letter.js
+++ Lucas-and-Liz/src/component/Letter.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import {isMobile} from 'mobile-device-detect';
 
 class Letter extends React.Component {
-    render() {
-      var letterStyle = {
-        padding: 10,
-        margin: 10,
-        backgroundColor: this.props.bgcolor,
-        color: "#333",
-        display: "inline-block",
-        fontFamily: "monospace",
-        fontSize: 32,
-        textAlign: "center"
-      }
-      return(
-        <div style={letterStyle}>
-          {this.props.children}
-        </div>
-      );
+  render() {
+    var letterStyle = {
+      padding: 10,
+      margin: 10,
+      backgroundColor: this.props.bgcolor,
+      color: "#333",
+      display: "inline-block",
+      fontFamily: "monospace",
+      fontSize: 32,
+      textAlign: "center"
     }
+    var mobileLetterStyle = {
+      padding: 10,
+      margin: 10,
+      backgroundColor: this.props.bgcolor,
+      color: "#333",
+      display: "inline-block",
+      fontFamily: "monospace",
+      fontSize: 26,
+      textAlign: "center"
+    }
+    return (
+      <div style={isMobile ? mobileLetterStyle : letterStyle}>
+        {this.props.children}
+      </div>
+    );
   }
+}
 
 export default Letter;
