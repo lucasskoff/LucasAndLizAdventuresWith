dad0412b782af1791c6c95925d317670dfb650de
lucasskoff <lucasskoff@gmail.com>
2019-09-06 21:23:42 -0500

    Fixed about site size and refactored to reduce duplicate code.

---
+++ Lucas-and-Liz/src/component/About.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './TextStyle.css';
+
+class AboutElizabeth extends Component {
+
+    render() {
+        var screenSize = {
+            width: 841,
+            height: 1367,
+        }
+        return (
+            <div style={this.screenSize}>
+                <div className='text' style={screenSize}>{this.props.name} is also cool!</div>
+            </div>
+        );
+    }
+}
+
+export default AboutElizabeth;
--- Lucas-and-Liz/src/component/App.js
+++ Lucas-and-Liz/src/component/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import {isMobile} from 'mobile-device-detect';
+import { isMobile } from 'mobile-device-detect';
 import Letter from './Letter';
 import Gallery from './Gallery';
-import AboutLucas from './AboutLucas';
-import AboutElizabeth from './AboutElizabeth';
+import About from './About';
 import NavBar from './NavBar';
 class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { page: "Gallery" }
+        const pages = ["Gallery", "About Lucas", "About Elizabeth"];
+        this.state = { page: "Gallery", width: 841, height:  1367 };
         this.changePage = this.changePage.bind(this);
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
     }
 
     createHeader() {
@@ -21,11 +26,11 @@ class App extends Component {
                 <Letter bgcolor="#FFD52E">C</Letter>
                 <Letter bgcolor="#49DD8E">A</Letter>
                 <Letter bgcolor="#AE99FF">S</Letter>
-                {isMobile ? <br/> : <Letter bgcolor="#333"> </Letter>}
+                {isMobile ? <br /> : <Letter bgcolor="#333"> </Letter>}
                 <Letter bgcolor="#FF605F">A</Letter>
                 <Letter bgcolor="#FFD52E">N</Letter>
                 <Letter bgcolor="#49DD8E">D</Letter>
-                {isMobile ? <br/> : <Letter bgcolor="#333"> </Letter>}
+                {isMobile ? <br /> : <Letter bgcolor="#333"> </Letter>}
                 <Letter bgcolor="#58B3FF">L</Letter>
                 <Letter bgcolor="#FF605F">I</Letter>
                 <Letter bgcolor="#FFD52E">Z</Letter>
@@ -43,12 +48,12 @@ class App extends Component {
 
     setPage(props) {
         const page = props;
-        if(page === "Gallery"){
-            return <Gallery/>
+        if (page === "Gallery") {
+            return <Gallery />
         } else if (page === "Lucas") {
-            return <AboutLucas/>
+            return <About screenWidth={this.state.width} screenHeight={this.state.height} name="Lucas"/>
         } else if (page === "Elizabeth") {
-            return <AboutElizabeth/>
+            return <About  screenWidth={this.state.width} screenHeight={this.state.height} name="Elizabeth"/>
         }
     }
 
