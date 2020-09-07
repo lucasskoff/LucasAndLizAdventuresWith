29deeb92a840c4d347d3b0e20fd3b94b9ce1f60a
lucasskoff <lucasskoff@gmail.com>
2020-09-07 00:04:28 -0500

    added video

--- Lucas-and-Liz/src/component/PhotoGrid.js
+++ Lucas-and-Liz/src/component/PhotoGrid.js
@@ -76,6 +76,7 @@ class Gallery extends Component {
         
         return (
             <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/m6lk_TkKP9Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 {albums.map((photo) =>
                     <PhotoContainer
                         key={photo.id}
